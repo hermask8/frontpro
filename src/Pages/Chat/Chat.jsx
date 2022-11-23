ffadd8ffcc667049ade6c644361f1737c50156d5
hermask8 <hermask8@gmail.com>
2022-11-23 17:29:51 -0600

    Chat

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -34,7 +34,7 @@ const Chat = () => {
 
   // Connect to Socket.io
   useEffect(() => {
-    socket.current = io("wss://chat-url.herokuapp.com");
+    socket.current = io("https://demo-socket-is.herokuapp.com");
     socket.current.emit("new-user-add", user._id);
     socket.current.on("get-users", (users) => {
       setOnlineUsers(users);
